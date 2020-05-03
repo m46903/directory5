dc3a593230340705433389e1fd3b793487b8336b
shahid <m46903@gmail.com>
2020-05-03 17:47:59 -0400

    deleted ec2+vpc+public_subnet directory

--- ec2+vpc+public_subnet/output.tf
+++
@@ -1,9 +0,0 @@
-output "indexers" {
-  value = [aws_instance.indexer.*.public_ip]
-}
-output "search_head" {
-  value = [aws_instance.search_head.*.public_ip]
-}
-output "cluster_master" {
-  value = [aws_instance.search_head.*.public_ip]
-}
