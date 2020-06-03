a644d0b20729a56f9db25d9dbcc465400729edbd
shahid mahmood <shahid.mahmood@accenturefederal.com>
2020-06-03 12:58:02 -0400

    new changes

---
+++ ec2_only.tf
@@ -0,0 +1,11 @@
+provider "aws" { region = "us-east-2" }
+
+resource "aws_instance" "ec2_only" {
+
+  ami           = "ami-01e36b7901e884a10" #Centos us-east-2
+  instance_type = "t2.micro"
+  key_name      = "ohio-content-cyber-range-aws-key"
+
+  tags = { Name = "ec2_with_default_vpc" }
+}
+
